479374aada9c1d12bcc8cff7ea2db6e326bba200
Shoichi Kaji <skaji@cpan.org>
2014-05-20 23:08:46 +0900

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# NAME
+
+Hello::World - new module
+
+# SYNOPSIS
+
+    use Hello::World;
+
+# DESCRIPTION
+
+Hello::World is a new module.
+
+# LICENSE
+
+Copyright (C) Shoichi Kaji.
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+# AUTHOR
+
+Shoichi Kaji <skaji@cpan.org>
---
+++ lib/Hello/World.pm
@@ -0,0 +1,37 @@
+package Hello::World;
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = '0.001';
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Hello::World - new module
+
+=head1 SYNOPSIS
+
+    use Hello::World;
+
+=head1 DESCRIPTION
+
+Hello::World is a new module.
+
+=head1 LICENSE
+
+Copyright (C) Shoichi Kaji.
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Shoichi Kaji E<lt>skaji@cpan.orgE<gt>
+
+=cut
+
